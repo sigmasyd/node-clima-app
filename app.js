02f6e2360a857a789f9ba69910e6b17197ff1640
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-29 03:30:23 -0500

    Unificando lugar y clima
    	modificado:     app.js
    	modificado:     clima/clima.js

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const argv = require('./config/yargs').argv;
 
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
-
-clima.getClima(40.750000,-74.000000)
+ 
+const getInfo = async (direccion) => {
+  try{
+    const coords = await lugar.getLugarLatLng(direccion);
+    const temp = await clima.getClima(coords.lat,coords.lng);
+    return `El clima de ${coords.direccion} es de ${temp}`;
+  }catch(e){
+    console.log(e);
+    return `No se pudo determinar el clima de ${direccion}`;
+  }
+}
+  
+getInfo(argv.direccion)
   .then(console.log)
   .catch(console.log);
-
-// lugar.getLugarLatLng(argv.direccion)
-//   .then(console.log);
-
-
-
-  
--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 const getClima = async (lat,lng) => {
-  const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=7c32305720b8a75d1143be94377b9338&units=metric`);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=7c32305720b8a75d1143be94377b9338&units=metric`;
+  const resp = await axios.get(url);
   return resp.data.main.temp;
 }
 
